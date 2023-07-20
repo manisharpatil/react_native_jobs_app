0cd85e0e97acb0980d878c87f250b57e20d3a231
Manisha Patil <manisharpatil6@gmail.com>
2023-07-20 22:48:45 +0530

    initials commits

--- app/index.js
+++ app/index.js
@@ -1,4 +1,11 @@
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from './constants';
+import {
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
+} from '../components';
 
 const Home = () => {
     return (
