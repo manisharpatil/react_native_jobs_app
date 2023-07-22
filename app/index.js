4b4a265d933fc585ef3be45e227ba7ec51c6bc51
Manisha Patil <manisharpatil6@gmail.com>
2023-07-22 16:54:28 +0530

    fix: resolve bug for missing file path

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import { COLORS, icons, images, SIZES } from './constants';
+import { COLORS, icons, images, SIZES } from '../constants';
 import {
     Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome
 } from '../components';
@@ -12,8 +12,30 @@ const Home = () => {
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen 
-            options={{ headerStyle: { backgroundColor: COLORS.lightWhite}}}/>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }} />
+
+            <ScrollView showVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 };
