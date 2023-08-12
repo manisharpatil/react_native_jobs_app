787cf5e28908be24fbf8b02bc70aaca754042e82
Manisha Patil <manisharpatil6@gmail.com>
2023-08-12 15:46:54 +0530

    Feat: Added NearByJobCard component

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import styles from "../../../home/popular/popularjobs.style";
+
+import styles from "./popularjobcard.style";
 import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
@@ -11,8 +11,12 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"}}
-          resizeMode="contain"
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode='contain'
           style={styles.logoImage}
         />
       </TouchableOpacity>
@@ -20,12 +24,17 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         {item.employer_name}
       </Text>
 
-      <view style={styles.infoContainer}>
+      <View style={styles.infoContainer}>
         <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
           {item.job_title}
         </Text>
-        <Text style={styles.location}>{item.job_country}</Text>
-      </view>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 };
