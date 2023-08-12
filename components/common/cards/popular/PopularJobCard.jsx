746dbc28b0e606622767a91c75387cbf6e249cf6
Manisha Patil <manisharpatil6@gmail.com>
2023-08-12 15:20:38 +0530

    feat: NearbyJob component added with PopularJobCaard and PopularJobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,21 +1,33 @@
-
-import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
-import styles from '../../../home/popular/popularjobs.style';
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import styles from "../../../home/popular/popularjobs.style";
+import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
     <TouchableOpacity
-    style={styles.container(selectedJob, item)}
-    onPress={() => handleCardPress(item)}>
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-        <Image 
-          source={{uri: item.employer_logo }}
+        <Image
+          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"}}
           resizeMode="contain"
-          style={styles.logoImage}/>
+          style={styles.logoImage}
+        />
       </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+
+      <view style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </view>
     </TouchableOpacity>
-  )
+  );
 };
 
 export default PopularJobCard;
